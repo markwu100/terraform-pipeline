250dc53c7f34355cab1e7cbcde42b2e69025bc16
markwu100 <156709406@qq.com>
2019-03-28 23:26:40 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-01ed306a12b7d1c96"
-  instance_type = "t2.medium"
+  ami           = "ami-04481c741a0311bbb"
+  instance_type = "t2.micro"
 }
